c0aaa8f61f697a78ee7664dea48de2ace08cdb1c
Ivan WeiXian Li <dave159159@yahoo.com>
2020-02-16 21:17:54 -0800

    Removed main activity floating button text

--- app/src/main/java/com/example/ivanwl/specifit/MainActivity.java
+++ app/src/main/java/com/example/ivanwl/specifit/MainActivity.java
@@ -56,8 +56,10 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                /*
                 Snackbar.make(view, "Location Updated", Snackbar.LENGTH_SHORT)
                         .setAction("Action", null).show();
+                 */
                 goToRestaurants();
             }
         });
